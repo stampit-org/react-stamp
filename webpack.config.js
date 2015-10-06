91548fb9da5f50691add9f4209303736acf92345
Tim Routowicz <troutowicz@gmail.com>
2015-10-05 22:09:25 -0400

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve('./dist'),
     filename: minify ? '[name].min.js' : '[name].js',
-    library: 'react-stamp',
+    library: 'reactStamp',
     libraryTarget: 'umd',
   },
 
