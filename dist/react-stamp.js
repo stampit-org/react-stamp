b5e3923e733eaeaf472a380939e7645c5bafd9d5
Tim Routowicz <troutowicz@gmail.com>
2015-10-05 22:11:19 -0400

    Rebuild dist

--- dist/react-stamp.js
+++ dist/react-stamp.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["react-stamp"] = factory();
+		exports["reactStamp"] = factory();
 	else
-		root["react-stamp"] = factory();
+		root["reactStamp"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
